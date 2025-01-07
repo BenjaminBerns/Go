9c3afef1d855f0c325af7b95e51c8bd23e108bc0
Benji <benjixard@gmail.Com>
2025-01-07 12:10:15 +0100

    push 12:10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { BoardComponent } from './board/board.component';
 import { CelluleComponent } from './cellule/cellule.component';
+import { NgClass, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, BoardComponent, CelluleComponent],
+  imports: [RouterOutlet, RouterModule, NgFor, NgClass, FormsModule, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'jeu-de-go';
+ 
 }
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,11 +1,41 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [RouterOutlet, RouterModule, NgFor, NgClass, FormsModule, BoardComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
 export class BoardComponent {
+  title = 'jeu-de-go';
+  table: number[] = Array.from({ length: 9 });
+  table2: number[] = Array.from({ length: 9 });
 
+  tour: number = 0;
+  isBackgroundBlack: boolean = false;
+
+  test(): void{
+    this.tour = 5;
+  }
+
+  Class(): string {
+    if (this.tour === 1) {
+      return 'black';  
+    }
+    if (this.tour === 2) {
+      return 'white';  
+    }
+    return '';  
+  }
+
+  Changement(): void {
+    if (this.tour === 1 || this.tour === 0) {
+      this.tour = 2;  
+    } else if (this.tour === 2) {
+      this.tour = 1; 
+    }
+  }
 }
